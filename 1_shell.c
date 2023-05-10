476c1bb9b5aca09e80decbd8ce8eb73374eab4b7
Prince  Magutshwa <prince@schematech.co.za>
2023-05-10 18:46:14 +0200

    Simple shell 0.1
    UNIX command line interpreter skeleton.

---
+++ 1_shell.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - it runs the program of simple shell 
+*
+* Return: 0 upon finshing with success
+*
+*/
+int main(void)
+{
+    //Code here
+    char *userFilePath = NULL;
+	return (0);
+}
