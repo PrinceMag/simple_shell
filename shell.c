472711eeac995f86aaa6eee511d031840d57770f
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-13 16:15:22 +0100

    Add description for shell.c

--- shell.c
+++ shell.c
@@ -1,11 +1,19 @@
 #include "shell.h"
 
+/**
+* main - entry point.
+* @argc: argument counter.
+* @argv: argument argument
+* @envVar: env variables.
+* Return: (0).
+*/
+
 int main(int argc, char **argv, char **envVar)
 {
-    if (argc == 1)
-    {
-        cmdPrompt(argv, envVar);
-    }
+	if (argc == 1)
+	{
+		cmdPrompt(argv, envVar);
+	}
 
-    return 0;
+	return (0);
 }
